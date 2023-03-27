47a0d6d7a5c33fbbcdee7bb1738931dccdc1611f
schef <zvanstefan@gmail.com>
2023-03-27 16:10:11 +0200

    added masterclass

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@
 from connector import Connector
 from organ import Organ
 from midi_player import MidiPlayer
-from time import sleep
 
 organ = Organ()
 player = MidiPlayer()
--- src/pitch_parser.py
+++ src/pitch_parser.py
@@ -33,58 +33,5 @@ class Pitches(Pitch, Enum):
     
 
 class PitchParser:
-    MIDI_BASE = 48
+    MIDI_BASE = 60
 
-    def is_midi_pitch(self, midi, pitch):
-        pitch = re.sub('[\',]', '', pitch)
-        return midi % 12 == self.pitchMidiBase[pitch]
-
-    def get_pitch_base(self, pitch):
-        pitch = re.sub('[\',]', '', pitch)
-        return pitch
-
-    def get_octave_from_pitch(self, pitch):
-        octave = 0
-        for i in pitch:
-            if i == "'":
-                octave += 1
-            elif i == ",":
-                octave -= 1
-        return self.MIDI_BASE + octave * 12
-
-    def get_midi_from_pitch(self, pitch):
-        octave = self.get_octave_from_pitch(pitch)
-        pitch = re.sub('[\',]', '', pitch)
-        return self.pitchMidiBase[pitch] + octave
-
-    def get_octave_from_midi(self, midi):
-        rest = midi % 12
-        center = int(self.MIDI_BASE / 12)
-        octave = int((midi - rest) / 12)
-        centerOctave = (octave - center)
-        if centerOctave > 0:
-            return "'" * centerOctave
-        elif centerOctave < 0:
-            return "," * abs(centerOctave)
-        else:
-            return ""
-
-    def get_pitch_from_midi(self, midi):
-        octave = self.get_octave_from_midi(midi)
-        for name, midiBase in self.pitchMidiBase.items():
-            if midiBase == midi % 12:
-                return name + octave
-        return ''
-
-    def get_midi_list_from_pitch_list(self, pitchList):
-        midiList = []
-        for pitch in pitchList:
-            midiList.append(self.get_midi_from_pitch(pitch))
-        return midiList
-
-    def get_midi_base_from_midi(self, midi):
-        return midi % 12
-
-    def get_midi_base_from_pitch(self, pitch):
-        midi = self.get_midi_from_pitch(pitch)
-        return self.get_midi_base_from_midi(midi)
